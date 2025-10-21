b7f4a67a3db226a0aecae2b263048c890b8557ec
shenpeng <396635614@qq.com>
2025-10-21 16:18:17 +0800

    feat：修改选集展示逻辑，不在只展示数字，而展示具体标题

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,13 +1,8 @@
-const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-    }
-};
+const CUSTOMER_SITES = {}
 
 // 调用全局方法合并
 if (window.extendAPISites) {
-    window.extendAPISites(CUSTOMER_SITES);
+  window.extendAPISites(CUSTOMER_SITES)
 } else {
-    console.error("错误：请先加载 config.js！");
+  console.error("错误：请先加载 config.js！")
 }
